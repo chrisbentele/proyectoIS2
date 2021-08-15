24a4fbf7488ea8cc70517e29df52be81c981d3e7
christian bentele <christianbentelem@gmail.com>
2021-08-15 17:56:28 +0200

    First commit with React

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>App para IS2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
