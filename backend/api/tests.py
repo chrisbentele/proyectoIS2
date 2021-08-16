b31453b9513317c656ca50d65cedf773116a21be
ete99 <stefanvallet99@gmail.com>
2021-08-16 11:03:55 -0400

    bug fix

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,9 +3,9 @@ from django.test import TestCase
 # Create your tests here.
 class URLTests(TestCase):
     def test_testprotectoindex(self):
-        res = self.client.get("/proyecto/")
+        res = self.client.get("/api/proyecto/")
         self.assertEqual(res.status_code, 200)
 
     def test_testproyectonamed(self):
-        res = self.client.get("/proyecto/1/")
+        res = self.client.get("/api/proyecto/1")
         self.assertEqual(res.status_code, 200)
