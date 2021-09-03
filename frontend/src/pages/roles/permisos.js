e47d6b42877aead2a62192686cb05c548c9d85a8
MatiasLGonzalez <matias041198@gmail.com>
2021-09-03 00:35:34 -0400

    Create roles branch

---
+++ frontend/src/pages/roles/permisos.js
@@ -0,0 +1,30 @@
+export const permisos = {
+  Categorias: {
+    Proyecto: [
+      "Crear proyecto",
+      "Ver proyecto",
+      "Editar configuración del proyecto",
+      "Editar miembros a proyecto",
+      "Eliminar proyecto"
+    ],
+    US: [
+      "Crear US",
+      "Modificar US",
+      "Eliminar US",
+      "Estimar US",
+      "Asignar miembros a US",
+      "Cambiar estado US"
+    ],
+    Roles: [
+      "Crear rol",
+      "Editar parámetros del rol",
+      "Eliminar rol",
+      "Asignar rol"
+    ],
+    Usuario: [
+      "Agregar usuario",
+      "Editar rol del usuario",
+      "Eliminar usuario"
+    ]
+  }
+}
