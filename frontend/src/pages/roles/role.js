3822c939197db212d3a505715cae1f61ffc72bf2
MatiasLGonzalez <matias041198@gmail.com>
2021-09-03 13:57:58 -0400

    Added Roles

--- frontend/src/pages/roles/role.js
+++ frontend/src/pages/roles/role.js
@@ -2,18 +2,11 @@ import { useState } from 'react';
 import {
   Box,
   FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
   Input,
-  Radio,
-  Stack,
-  RadioGroup,
   Checkbox,
   CheckboxGroup,
   Select,
-  Grid, GridItem,
-  Button, ButtonGroup
+  Grid, Button
 } from "@chakra-ui/react";
 import { PERMISOS } from "./permisos";
 import React from "react";
@@ -22,18 +15,26 @@ import { useForm } from 'react-hook-form';
 export const Role = () => {
   const [rol, setRol] = useState();
   const [add, setAdd] = useState();
-  let roles = [{ title: 'Admin', permisos: PERMISOS.map((x) => x.value) }, { title: 'Scrum Master', permisos: [1, 2] }];
+  let roles = [
+    { title: 'Admin', permisos: PERMISOS.map((x) => x.value) },
+    { title: 'Scrum Master', permisos: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] },
+    { title: 'Developer team', permisos: [1, 2, 5, 6, 7, 8, 10, 11] }
+  ];
   const { register, handleSubmit, watch, formState: { errors }, setValue } = useForm();
   const onSubmit = data => console.log(data);
-  console.log(watch("example")); // watch input value by passing the name of it
+  //console.log(watch("example")); // watch input value by passing the name of it
+  //value={x.value.toString()}
 
-  console.log(rol);
-  console.log(rol?.permisos.includes(0));
+  //console.log(rol);
+  //console.log(rol?.permisos.includes(0));
+  //{PERMISOS.map(x => (console.log(rol?.permisos.includes(x.value))))}
+  //{add ? x.value.toString():null}
   return (
     <Box>
       <Select onChange={(e) => setRol(roles[e.target.value])}>
         <option hidden>Seleccione un rol</option>
-        {roles.map((x, i) => (<option key={i} value={i} onClick={() => setAdd(false)}>{x.title}</option>))}
+        {roles.map((x, i) => (
+          <option key={i} value={i} onClick={() => setAdd(false)}>{x.title}</option>))}
         <option onClick={() => setAdd(true)}>Agregar</option>
       </Select>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -41,7 +42,13 @@ export const Role = () => {
         <FormControl>
           <CheckboxGroup defaultValue="Itachi" onChange={(val) => setValue("ete", val)}>
             <Grid templateColumns="repeat(5, 1fr)" gap={6} padding="10">
-              {PERMISOS.map(x => (<Checkbox value={x.value.toString()} isDisabled={'Admin' === rol?.title || (!rol && !add)} isChecked={rol?.permisos.includes(x.value)}>{x.title}</Checkbox>))}
+              {PERMISOS.map(x => (
+                <Checkbox
+                  value={add ? x.value.toString() : undefined}
+                  isDisabled={'Admin' === rol?.title || (!rol && !add)}
+                  isChecked={rol?.permisos.includes(x.value)}>
+                  {x.title}
+                </Checkbox>))}
             </Grid>
           </CheckboxGroup>
         </FormControl>
