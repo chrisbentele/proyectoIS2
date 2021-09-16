556e73189ca1bfae8a273476eab53f3b5a5ab845
christian bentele <christianbentelem@gmail.com>
2021-09-16 22:46:52 +0200

    Create project now working

--- frontend/src/api/projects.js
+++ frontend/src/api/projects.js
@@ -1,9 +1,23 @@
 import { axiosInstance } from ".";
 
 //Projects
-export const createProject = async (projectName, scrumMasterId) => {
+export const createProject = async (projectData) => {
+  console.log(projectData);
+  const { projectName, scrumMasterId, estimation } = projectData;
+  console.log(`estimation: ${estimation}`);
+  console.log(`scrum master id: ${scrumMasterId}`);
+  console.log(`project name:${projectName}`);
+  if (!estimation || !scrumMasterId || !projectName) {
+    return new Error(
+      "Para crear proyecto debes proveer la duracion estimada, miembros y nombre del proyecto"
+    );
+  }
   try {
-    const res = await axiosInstance.post(projectName, scrumMasterId);
+    const res = await axiosInstance.post("proyectos", {
+      duracionEstimada: estimation,
+      miembros: [scrumMasterId],
+      nombre: projectName,
+    });
     return res.data;
   } catch (error) {
     return error;
