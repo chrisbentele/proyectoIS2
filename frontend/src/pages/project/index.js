2ff3f5d419dd0251351970a027352d281a39d022
Christian Bentele <christianbentelem@gmail.com>
2021-09-20 00:16:53 +0200

    Added: user stories api

--- frontend/src/pages/project/index.js
+++ frontend/src/pages/project/index.js
@@ -6,15 +6,20 @@ import { Link } from "react-router-dom";
 
 export default function Index({ props }) {
   const projectId = props.computedMatch.params.id; //id del proyecto, se extrae del URL
-  const [project, setProject] = useState({}); //estado del proyecto
+  const [project, setProject] = useState({ userStories: [] }); //estado del proyecto
 
   //Al cargarse la pagina se busca el proyecto con el id del URL y se lo asigna a projectId
   useEffect(() => {
     api
       .getProjectById(projectId)
-      .then((project) => setProject(project))
+      .then((res) => setProject({ ...project, ...res }))
+      .catch((err) => console.log(err));
+    api
+      .getUserStories(projectId)
+      .then((US) => setProject({ ...project, userStories: US }))
       .catch((err) => console.log(err));
   }, []);
+  console.log(project);
   return (
     <Box
       minHeight="100vh"
@@ -31,6 +36,9 @@ export default function Index({ props }) {
           <Heading>
             <Link to={`${projectId}/members`}>Miembros</Link>
           </Heading>
+          {project.userStories
+            ? project.userStories.map((us) => <p>{us.nombre}</p>)
+            : null}
         </Box>
       ) : (
         <Spinner size="xl" />
