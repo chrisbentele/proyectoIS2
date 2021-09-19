11092b5de8bb585bab033d2ff7238966e5b03eb9
Fabrizio Sorrentino <fabri.ss98@gmail.com>
2021-09-19 15:05:45 -0400

    a

--- frontend/src/pages/proyecto.js
+++ frontend/src/pages/proyecto.js
@@ -2,30 +2,32 @@ import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-    const { user, isAuthenticated,isLoading, getAccessTokenSilently } = useAuth0();
-    const [proyecto,setProyecto] = useState()
-    const [data ,setData] = useState()
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
+    useAuth0();
+  const [proyecto, setProyecto] = useState();
+  const [data, setData] = useState();
 
-  useEffect(()=>{
-      const getProyecto = async() =>{
-        const token =  await getAccessTokenSilently()
-        console.log(token)
-          const data =await fetch("http://localhost:8000/api/proyecto",{headers:{"auth-token":token}})
-          setData(data)
-          console.log(data)
-      }
-      if (isAuthenticated){
-        getProyecto()
-      }
-    setProyecto()
-  },[isLoading])
+  useEffect(() => {
+    const getProyecto = async () => {
+      const token = await getAccessTokenSilently();
+      console.log(token);
+      const data = await fetch("http://localhost:8000/api/proyecto", {
+        headers: { "auth-token": token },
+      });
+      setData(data);
+      console.log(data);
+    };
+    if (isAuthenticated) {
+      getProyecto();
+    }
+    setProyecto();
+  }, [isLoading]);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
   return (
     isAuthenticated && (
       <div>
-        
         {JSON.stringify(data)}
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
