6320bfc1c47e8358b036825ebe3ab2269b485f9a
Fabrizio Sorrentino <fabri.ss98@gmail.com>
2021-09-19 18:23:36 -0400

    pre-merge para kanban

--- frontend/src/pages/project/index.js
+++ frontend/src/pages/project/index.js
@@ -1,20 +1,26 @@
+  
 import React, { useEffect, useState } from "react";
 import { api } from "../../api";
 import { Spinner } from "@chakra-ui/spinner";
-import { Box, Heading, Flex } from "@chakra-ui/layout";
+import { Box, Heading } from "@chakra-ui/layout";
 import { Link } from "react-router-dom";
 
 export default function Index({ props }) {
   const projectId = props.computedMatch.params.id; //id del proyecto, se extrae del URL
-  const [project, setProject] = useState({}); //estado del proyecto
+  const [project, setProject] = useState({ userStories: [] }); //estado del proyecto
 
   //Al cargarse la pagina se busca el proyecto con el id del URL y se lo asigna a projectId
   useEffect(() => {
     api
       .getProjectById(projectId)
-      .then((project) => setProject(project))
+      .then((res) => setProject({ ...project, ...res }))
+      .catch((err) => console.log(err));
+    api
+      .getUserStories(projectId)
+      .then((US) => setProject({ ...project, userStories: US }))
       .catch((err) => console.log(err));
   }, []);
+  console.log(project);
   return (
     <Box
       minHeight="100vh"
@@ -22,52 +28,22 @@ export default function Index({ props }) {
       bg={"#F5F4F5"}
       color="#2b2d42"
       d="flex"
-      justifyContent="left"
+      justifyContent="center"
+      alignItems="center"
     >
       {project ? ( //si ya se cargo el proyecto se muestra el mismo, si no se muestra la pantalla de carga
-        <Box
-          pos="fixed"
-          top="50px"
-          zIndex="100"
-          bg={"#F7FFF7"}
-          left="0"
-          right="0"
-          // boxShadow="md"
-          width="full"
-          p="5px"
-          mb="3rem"
-        >
-          <Flex justifyContent="left">
-            <Box>hola</Box>
-
-            <Box></Box>
-
-            <Box>
-              {/* <Link to="/projects">Projects</Link> */}
-              <Heading>ID proyecto{project.nombre}</Heading>
-            </Box>
-            <Heading>
-              <Link to={`${projectId}/members`}>Miembros</Link>
-            </Heading>
-          </Flex>
+        <Box width="50%" borderWidth="5px" bg="#E2E8F0" height="50%">
+          <Heading>{project.nombre}</Heading>
+          <Heading>
+            <Link to={`${projectId}/members`}>Miembros</Link>
+          </Heading>
+          {project.userStories
+            ? project.userStories.map((us) => <p>{us.nombre}</p>)
+            : null}
         </Box>
       ) : (
         <Spinner size="xl" />
       )}
-      <Flex
-        w="xs"
-        height="200px"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        fontSize="3xl"
-        fontWeight="bold"
-        bg="white"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Link to="/createProject/">Card</Link>
-      </Flex>
     </Box>
   );
 }
