c1ad81cf6e979032ca2bab6f24b6e83e4773be3b
christian bentele <christianbentelem@gmail.com>
2021-09-19 14:20:11 +0200

    Listar proyectos en profile

---
+++ frontend/src/pages/login/index.js
@@ -0,0 +1,13 @@
+import { Center } from "@chakra-ui/react";
+import React from "react";
+import LoginButton from "../../components/auth/loginButton/loginButton";
+import "./login.css";
+export default function Login() {
+  return (
+    <div className="loginContainer">
+      <Center bg="white" h="100px" color="black">
+        <LoginButton />
+      </Center>
+    </div>
+  );
+}
