9b518988d64724bf05fea5c60104ed5c3fed9d35
Fabrizio Sorrentino <fabri.ss98@gmail.com>
2021-09-19 17:27:15 -0400

    cambios a pagina de proyecto

--- frontend/src/pages/project/index.js
+++ frontend/src/pages/project/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { api } from "../../api";
 import { Spinner } from "@chakra-ui/spinner";
-import { Box, Heading } from "@chakra-ui/layout";
+import { Box, Heading, Flex } from "@chakra-ui/layout";
 import { Link } from "react-router-dom";
 
 export default function Index({ props }) {
@@ -25,8 +25,31 @@ export default function Index({ props }) {
       justifyContent="center"
       alignItems="center"
     >
+      <Box
+        pos="fixed"
+        top="50px"
+        zIndex="100"
+        bg={"#F7FFF7"}
+        left="0"
+        right="0"
+        // boxShadow="md"
+        width="full"
+        p="5px"
+        mb="3rem"
+      >
+        <Flex justifyContent="left">
+          <Box>hola</Box>
+
+          <Box></Box>
+
+          <Box>
+            {/* <Link to="/projects">Projects</Link> */}
+            <Heading>ID proyecto</Heading>
+          </Box>
+        </Flex>
+      </Box>
       {project ? ( //si ya se cargo el proyecto se muestra el mismo, si no se muestra la pantalla de carga
-        <Box width="50%" borderWidth="5px" bg="#E2E8F0" height="50%">
+        <Box width="180px" borderWidth="5px" bg="#E2E8F0" height="50%">
           <Heading>{project.nombre}</Heading>
           <Heading>
             <Link to={`${projectId}/members`}>Miembros</Link>
@@ -35,6 +58,20 @@ export default function Index({ props }) {
       ) : (
         <Spinner size="xl" />
       )}
+      <Flex
+        w="xs"
+        height="200px"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        fontSize="3xl"
+        fontWeight="bold"
+        bg="white"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Link to="/createProject/">Crear Proyecto</Link>
+      </Flex>
     </Box>
   );
 }
