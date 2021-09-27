2c2e3f1446e9969f6117f02b486ab411d902915f
ete99 <stefanvallet99@gmail.com>
2021-09-26 22:40:04 -0400

    rm utils module & bug fix

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,7 +1,8 @@
 import json
-from api.models import Proyecto, Usuario
+import uuid
 from api.serializers import (
     ProyectoSerializer,
+    RolAsignadoSerializer,
     RolSerializer,
     USSerializer,
     UsuarioSerializer,
@@ -9,38 +10,44 @@ from api.serializers import (
 from django.test import TestCase
 
 
-def crear_proyecto(miembros=[]):
-    proy_serializer = ProyectoSerializer(
-        data={"duracionEstimada": 1, "miembros": miembros, "nombre": "test"}
-    )
-    if proy_serializer.is_valid():
-        proyecto = proy_serializer.save()
-
-    return proyecto
-
-
 def crear_user():
     user_serializer = UsuarioSerializer(
-        data={"nombre": "ete", "email": "ete@etemail.com"}
+        data={"nombre": "ete", "email": str(uuid.uuid4()) + "@etemail.com"}
     )
     if user_serializer.is_valid():
-        user = user_serializer.save()
+        user_serializer.save()
+    return user_serializer.data
 
-    return user
+
+def crear_proyecto(self, miembros=None):
+    miembros = [str(crear_user()["id"])]
+    res = self.client.post(
+        f"/api/proyectos",
+        json.dumps(
+            {
+                "duracionEstimada": 1,
+                "miembros": miembros,
+                "scrumMasterId": miembros[0],
+                "nombre": "test",
+            }
+        ),
+        content_type="application/json",
+    )
+    return res.json()
 
 
 def crear_rol(proyect_id):
-    user_serializer = RolSerializer(
+    seri = RolSerializer(
         data={"nombre": "ete", "proyecto": proyect_id, "permisos": [1, 2]}
     )
-    if user_serializer.is_valid():
-        user = user_serializer.save()
+    if seri.is_valid():
+        seri.save()
 
-    return user
+    return seri.data
 
 
 def crear_US(proyect_id, user_id):
-    user_serializer = USSerializer(
+    seri = USSerializer(
         data={
             "proyecto": proyect_id,
             "nombre": "ete",
@@ -48,10 +55,10 @@ def crear_US(proyect_id, user_id):
             "creadoPor": user_id,
         }
     )
-    if user_serializer.is_valid():
-        user = user_serializer.save()
+    if seri.is_valid():
+        seri.save()
 
-    return user
+    return seri.data
 
 
 class Proyectos_Tests(TestCase):
@@ -60,33 +67,42 @@ class Proyectos_Tests(TestCase):
         self.assertEqual(res.status_code, 200)
 
     def test_proyectos_create(self):
-
+        miembros = [str(crear_user()["id"])]
         res = self.client.post(
             f"/api/proyectos",
-            json.dumps({"duracionEstimada": 1, "miembros": [], "nombre": "test"}),
+            json.dumps(
+                {
+                    "duracionEstimada": 1,
+                    "miembros": miembros,
+                    "scrumMasterId": miembros[0],
+                    "nombre": "test",
+                }
+            ),
             content_type="application/json",
         )
+
         self.assertEqual(res.status_code, 201)
 
     def test_proyectos_get(self):
 
-        r = crear_proyecto()
-        res = self.client.get(f"/api/proyectos/{r.id}")
+        r = crear_proyecto(self)
+
+        res = self.client.get(f"/api/proyectos/{r['id']}")
         self.assertEqual(res.status_code, 200)
 
     def test_proyectos_delete(self):
 
-        r = crear_proyecto()
+        r = crear_proyecto(self)
 
-        res = self.client.delete(f"/api/proyectos/{r.id}")
+        res = self.client.delete(f"/api/proyectos/{r['id']}")
         self.assertEqual(res.status_code, 204)
 
     def test_proyectos_update(self):
 
-        r = crear_proyecto()
+        r = crear_proyecto(self)
 
         res = self.client.put(
-            f"/api/proyectos/{r.id}",
+            f"/api/proyectos/{r['id']}",
             json.dumps({"nombre": "testeado"}),
             content_type="application/json",
         )
@@ -107,14 +123,14 @@ class Usuarios_Tests(TestCase):
 
         r = crear_user()
 
-        res = self.client.get(f"/api/usuarios/{r.id}")
+        res = self.client.get(f"/api/usuarios/{r['id']}")
         self.assertEqual(res.status_code, 200)
 
     def test_usuarios_delete(self):
 
         r = crear_user()
 
-        res = self.client.delete(f"/api/usuarios/{r.id}")
+        res = self.client.delete(f"/api/usuarios/{r['id']}")
         self.assertEqual(res.status_code, 204)
 
     def test_usuarios_update(self):
@@ -122,7 +138,7 @@ class Usuarios_Tests(TestCase):
         r = crear_user()
 
         res = self.client.put(
-            f"/api/usuarios/{r.id}",
+            f"/api/usuarios/{r['id']}",
             json.dumps({"nombre": "testeado"}),
             content_type="application/json",
         )
@@ -131,9 +147,9 @@ class Usuarios_Tests(TestCase):
     def test_usuarios_proyectos(self):
 
         r = crear_user()
-        # p = crear_proyecto([r.id])
+        # p = crear_proyecto([r['id']])
 
-        res = self.client.get(f"/api/usuarios/{r.id}/proyectos")
+        res = self.client.get(f"/api/usuarios/{r['id']}/proyectos")
 
         self.assertEquals(res.status_code, 200)
 
@@ -141,9 +157,9 @@ class Usuarios_Tests(TestCase):
 class Proyectos_Miembros_Tests(TestCase):
     def test_proyectos_miembros_add(self):
         u = crear_user()
-        p = crear_proyecto()
+        p = crear_proyecto(self)
         res = self.client.post(
-            f"/api/proyectos/{p.id}/miembros/{u.id}",
+            f"/api/proyectos/{p['id']}/miembros/{u['id']}",
             # json.dumps({"nombre": "ete", "email": "ete@etemail.com"}),
             # content_type="application/json",
         )
@@ -151,26 +167,26 @@ class Proyectos_Miembros_Tests(TestCase):
 
     def test_proyectos_miembros_get(self):
 
-        p = crear_proyecto()
+        p = crear_proyecto(self)
 
-        res = self.client.get(f"/api/proyectos/{p.id}/miembros")
+        res = self.client.get(f"/api/proyectos/{p['id']}/miembros")
         self.assertEqual(res.status_code, 200)
 
     def test_proyectos_miembros_remove(self):
 
         u = crear_user()
-        p = crear_proyecto([u.id])
+        p = crear_proyecto(self, [u["id"]])
 
-        res = self.client.delete(f"/api/proyectos/{p.id}/miembros/{u.id}")
+        res = self.client.delete(f"/api/proyectos/{p['id']}/miembros/{u['id']}")
 
         self.assertEqual(res.status_code, 204)
 
 
 class Roles_Tests(TestCase):
     def test_roles_crear(self):
-        p = crear_proyecto()
+        p = crear_proyecto(self)
         res = self.client.post(
-            f"/api/proyectos/{p.id}/roles",
+            f"/api/proyectos/{p['id']}/roles",
             json.dumps({"nombre": "ete", "permisos": []}),
             content_type="application/json",
         )
@@ -178,28 +194,28 @@ class Roles_Tests(TestCase):
 
     def test_roles_get(self):
 
-        p = crear_proyecto()
-        r = crear_rol(p.id)
+        p = crear_proyecto(self)
+        r = crear_rol(p["id"])
 
-        res = self.client.get(f"/api/proyectos/{p.id}/roles/{r.id}")
+        res = self.client.get(f"/api/proyectos/{p['id']}/roles/{r['id']}")
 
         self.assertEqual(res.status_code, 200)
 
     def test_roles_delete(self):
 
-        p = crear_proyecto()
-        r = crear_rol(p.id)
+        p = crear_proyecto(self)
+        r = crear_rol(p["id"])
 
-        res = self.client.delete(f"/api/proyectos/{p.id}/roles/{r.id}")
+        res = self.client.delete(f"/api/proyectos/{p['id']}/roles/{r['id']}")
 
         self.assertEqual(res.status_code, 204)
 
     def test_roles_update(self):
 
-        p = crear_proyecto()
-        r = crear_rol(p.id)
+        p = crear_proyecto(self)
+        r = crear_rol(p["id"])
         res = self.client.put(
-            f"/api/proyectos/{p.id}/roles/{r.id}",
+            f"/api/proyectos/{p['id']}/roles/{r['id']}",
             json.dumps({"nombre": "testeado"}),
             content_type="application/json",
         )
@@ -209,34 +225,36 @@ class Roles_Tests(TestCase):
 class Proyectos_Usuarios_Roles_Tests(TestCase):
     def test_proyectos_miembros_roles_add(self):
         u = crear_user()
-        p = crear_proyecto([u.id])
-        r = crear_rol(p.id)
+        p = crear_proyecto(self, [u["id"]])
+        r = crear_rol(p["id"])
 
         res = self.client.post(
-            f"/api/proyectos/{p.id}/miembros/{u.id}/roles/{r.id}",
+            f"/api/proyectos/{p['id']}/miembros/{u['id']}/roles/{r['id']}",
         )
         self.assertEqual(res.status_code, 201)
 
     def test_proyectos_miembros_roles_get(self):
 
         u = crear_user()
-        p = crear_proyecto([u.id])
+        p = crear_proyecto(self, [u["id"]])
 
-        res = self.client.get(f"/api/proyectos/{p.id}/miembros/{u.id}")
+        res = self.client.get(f"/api/proyectos/{p['id']}/miembros/{u['id']}")
 
         self.assertEqual(res.status_code, 200)
 
     def test_proyectos_miembros_roles_delete(self):
 
         u = crear_user()
-        p = crear_proyecto([u.id])
-        r = crear_rol(p.id)
+        p = crear_proyecto(self, [u["id"]])
+        r = crear_rol(p["id"])
 
         self.client.post(
-            f"/api/proyectos/{p.id}/miembros/{u.id}/roles/{r.id}",
+            f"/api/proyectos/{p['id']}/miembros/{u['id']}/roles/{r['id']}",
         )
 
-        res = self.client.delete(f"/api/proyectos/{p.id}/miembros/{u.id}/roles/{r.id}")
+        res = self.client.delete(
+            f"/api/proyectos/{p['id']}/miembros/{u['id']}/roles/{r['id']}"
+        )
 
         self.assertEqual(res.status_code, 204)
 
@@ -244,11 +262,11 @@ class Proyectos_Usuarios_Roles_Tests(TestCase):
 class user_stories(TestCase):
     def test_user_stories_crear(self):
         u = crear_user()
-        p = crear_proyecto([u.id])
+        p = crear_proyecto(self, [u["id"]])
 
         res = self.client.post(
-            f"/api/proyectos/{p.id}/user_stories",
-            json.dumps({"nombre": "ete", "contenido": "si", "creadoPor": u.id}),
+            f"/api/proyectos/{p['id']}/user_stories",
+            json.dumps({"nombre": "ete", "contenido": "si", "creadoPor": str(u["id"])}),
             content_type="application/json",
         )
         self.assertEqual(res.status_code, 201)
@@ -256,31 +274,31 @@ class user_stories(TestCase):
     def test_user_stories_get(self):
 
         u = crear_user()
-        p = crear_proyecto([u.id])
-        us = crear_US(p.id, u.id)
+        p = crear_proyecto(self, [u["id"]])
+        us = crear_US(p["id"], u["id"])
 
-        res = self.client.get(f"/api/proyectos/{p.id}/user_stories/{us.id}")
+        res = self.client.get(f"/api/proyectos/{p['id']}/user_stories/{us['id']}")
 
         self.assertEqual(res.status_code, 200)
 
     def test_user_stories_delete(self):
 
         u = crear_user()
-        p = crear_proyecto([u.id])
-        us = crear_US(p.id, u.id)
+        p = crear_proyecto(self, [u["id"]])
+        us = crear_US(p["id"], u["id"])
 
-        res = self.client.delete(f"/api/proyectos/{p.id}/user_stories/{us.id}")
+        res = self.client.delete(f"/api/proyectos/{p['id']}/user_stories/{us['id']}")
 
         self.assertEqual(res.status_code, 204)
 
     def test_user_stories_update(self):
 
         u = crear_user()
-        p = crear_proyecto([u.id])
-        us = crear_US(p.id, u.id)
+        p = crear_proyecto(self, [u["id"]])
+        us = crear_US(p["id"], u["id"])
 
         res = self.client.put(
-            f"/api/proyectos/{p.id}/user_stories/{us.id}",
+            f"/api/proyectos/{p['id']}/user_stories/{us['id']}",
             json.dumps({"nombre": "testeado"}),
             content_type="application/json",
         )
