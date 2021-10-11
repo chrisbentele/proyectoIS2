b5b855d9c9c074dccbe88de369e298839434b0c1
MatiasLGonzalez <matias041198@gmail.com>
2021-10-11 02:20:23 -0300

    Fixed modal

--- frontend/src/components/userStoryListUnset/userStoryListUnset.js
+++ frontend/src/components/userStoryListUnset/userStoryListUnset.js
@@ -220,71 +220,71 @@ const USListUnset = ({
                     />
                   )}
                   <Modal
-                    initialFocusRef={initialRef}
-                    isOpen={isOpenModalAssign}
-                    onClose={onCloseModalAssign}
-                  >
-                    <ModalOverlay />
-                    <ModalContent>
-                      <ModalHeader>Editar US</ModalHeader>
+                      initialFocusRef={initialRef}
+                      isOpen={isOpenModal}
+                      onClose={onCloseModalAssign}
+                    >
+                      <ModalOverlay />
+                      <ModalContent>
+                        <ModalHeader>Editar US</ModalHeader>
 
-                      <ModalCloseButton />
-                      <form onSubmit={handleSubmit(onSubmit)}>
-                        <ModalBody pb={6}>
-                          <FormControl isInvalid={errors.name}>
-                            <FormLabel htmlFor="name">Nombre US</FormLabel>
-                            <Input
-                              id="name"
-                              ref={initialRef}
-                              defaultValue={us.nombre}
-                              {...register("usName", {
-                                required: "This is required",
-                                minLength: {
-                                  value: 4,
-                                  message: "Minimum length should be 4",
-                                },
-                              })}
-                            />
-                            <FormErrorMessage>
-                              {errors.name && errors.name.message}
-                            </FormErrorMessage>
-                          </FormControl>
-                          <FormControl isInvalid={errors.description} mt={4}>
-                            <FormLabel htmlFor="description" mt={4}>
-                              Descripción
-                            </FormLabel>
-                            <Input
-                              id="description"
-                              defaultValue={us.contenido}
-                              {...register("description", {
-                                required: "This is required",
-                                minLength: {
-                                  value: 4,
-                                  message: "Minimum length should be 4",
-                                },
-                              })}
-                            />
-                            <FormErrorMessage>
-                              {errors.description &&
-                                errors.description.message}
-                            </FormErrorMessage>
-                          </FormControl>
-                        </ModalBody>
+                        <ModalCloseButton />
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                          <ModalBody pb={6}>
+                            <FormControl isInvalid={errors.name}>
+                              <FormLabel htmlFor="name">Nombre US</FormLabel>
+                              <Input
+                                id="name"
+                                ref={initialRef}
+                                defaultValue={us.nombre}
+                                {...register("usName", {
+                                  required: "This is required",
+                                  minLength: {
+                                    value: 4,
+                                    message: "Minimum length should be 4",
+                                  },
+                                })}
+                              />
+                              <FormErrorMessage>
+                                {errors.name && errors.name.message}
+                              </FormErrorMessage>
+                            </FormControl>
+                            <FormControl isInvalid={errors.description} mt={4}>
+                              <FormLabel htmlFor="description" mt={4}>
+                                Descripción
+                              </FormLabel>
+                              <Input
+                                id="description"
+                                defaultValue={us.contenido}
+                                {...register("description", {
+                                  required: "This is required",
+                                  minLength: {
+                                    value: 4,
+                                    message: "Minimum length should be 4",
+                                  },
+                                })}
+                              />
+                              <FormErrorMessage>
+                                {errors.description &&
+                                  errors.description.message}
+                              </FormErrorMessage>
+                            </FormControl>
+                          </ModalBody>
 
-                        <ModalFooter>
-                          <Button
-                            mr={4}
-                            colorScheme="blue"
-                            isLoading={isSubmitting}
-                            type="submit"
-                          >
-                            Guardar
-                          </Button>
-                          <Button onClick={onCloseModal}>Cancelar</Button>
-                        </ModalFooter>
-                      </form>
-                    </ModalContent>
-                  </Modal>
+                          <ModalFooter>
+                            <Button
+                              mr={4}
+                              colorScheme="blue"
+                              isLoading={isSubmitting}
+                              type="submit"
+                            >
+                              Guardar
+                            </Button>
+                            <Button onClick={onCloseModal}>Cancelar</Button>
+                          </ModalFooter>
+                        </form>
+                      </ModalContent>
+                    </Modal>
                   {canDelete ?
                     <Button
                       onClick={() => setIsOpen(true)}
