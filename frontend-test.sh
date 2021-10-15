5bcf3608a74a961d67e1f98ff4d67b933edef60d
MatiasLGonzalez <matias041198@gmail.com>
2021-10-14 23:39:52 -0300

    Adding frontend test script

---
+++ frontend-test.sh
@@ -0,0 +1,12 @@
+# Backend test script
+cd backend/ && \
+    sudo apt update && \
+    sudo apt install python3-pip python3-venv postgresql libpq-dev -y && \
+    mkdir .venv && \
+    python3 -m venv .venv && \
+    source .venv/bin/activate && \
+    python3 -m pip install -r requirements.txt && \
+    ./crear_db.sh && \
+    python3 manage.py migrate --run-syncdb && \
+    python3 manage.py runserver & \
+    cd ../frontend && yarn test
